15302bfe4d3b101f0a6fe9b7efd5a5526d7a2cc4
Tinz2 <tinapat2547@gmail.com>
2025-02-26 21:32:41 +0700

    แก้
    navigationbutton กับ แสดงชื่อ

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -106,65 +106,64 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.black,
       ),
       drawer: Drawer(
-  backgroundColor: Colors.black,
-  child: ListView(
-    padding: EdgeInsets.zero,
-    children: [
-      UserAccountsDrawerHeader(
-  decoration: BoxDecoration(
-    gradient: LinearGradient(
-      colors: [Colors.blue.shade900, Colors.blue.shade400],
-      begin: Alignment.topLeft,
-      end: Alignment.bottomRight,
-    ),
-  ),
-  accountName: Text(
-    _userData?['prefix'] != null
-        ? '${_userData!['username']}'
-        : 'ผู้ใช้: User',
-    style: TextStyle(color: Colors.white),
-  ),
-  accountEmail: Text(
-    _auth.currentUser?.email ?? 'อีเมล: Email',
-    style: TextStyle(color: Colors.white70),
-  ),
-  currentAccountPicture: CircleAvatar(
-    backgroundImage: _userData?['profileImage'] != null
-        ? NetworkImage(_userData!['profileImage'])
-        : AssetImage('assets/default_avatar.png') as ImageProvider,
-    child: _userData?['profileImage'] == null
-        ? Icon(Icons.camera_alt, color: Colors.white)
-        : null,
-  ),
-),
+        backgroundColor: Colors.black,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.blue.shade900, Colors.blue.shade400],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+              accountName: Text(
+                _userData?['username'] != null
+                    ? '${_userData!['username']}'
+                    : 'ผู้ใช้: User',
+                style: TextStyle(color: Colors.white),
+              ),
+              accountEmail: Text(
+                _auth.currentUser?.email ?? 'อีเมล: Email',
+                style: TextStyle(color: Colors.white70),
+              ),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: _userData?['profileImage'] != null
+                    ? NetworkImage(_userData!['profileImage'])
+                    : AssetImage('assets/default_avatar.png') as ImageProvider,
+                child: _userData?['profileImage'] == null
+                    ? Icon(Icons.camera_alt, color: Colors.white)
+                    : null,
+              ),
+            ),
 
-      // ปุ่มแก้ไขโปรไฟล์
-      ListTile(
-        tileColor: Colors.black,
-        leading: Icon(Icons.edit, color: Colors.white),
-        title: Text(
-          'แก้ไขโปรไฟล์: Edit Profile',
-          style: TextStyle(color: Colors.white),
+            // ปุ่มแก้ไขโปรไฟล์
+            ListTile(
+              tileColor: Colors.black,
+              leading: Icon(Icons.edit, color: Colors.white),
+              title: Text(
+                'แก้ไขโปรไฟล์: Edit Profile',
+                style: TextStyle(color: Colors.white),
+              ),
+              onTap: _navigateToProfileSetup,
+            ),
+            // ปุ่มออกจากระบบ
+            ListTile(
+              tileColor: Colors.black,
+              leading: Icon(Icons.logout, color: Colors.orange),
+              title: Text(
+                'ออกจากระบบ: Logout',
+                style: TextStyle(color: Colors.orange),
+              ),
+              onTap: () async {
+                await _auth.signOut(context);
+                Navigator.pushReplacementNamed(context, LoginPage.routeName);
+              },
+            ),
+          ],
         ),
-        onTap: _navigateToProfileSetup,
       ),
-      // ปุ่มออกจากระบบ
-      ListTile(
-        tileColor: Colors.black,
-        leading: Icon(Icons.logout, color: Colors.orange),
-        title: Text(
-          'ออกจากระบบ: Logout',
-          style: TextStyle(color: Colors.orange),
-        ),
-        onTap: () async {
-          await _auth.signOut(context);
-          Navigator.pushReplacementNamed(context, LoginPage.routeName);
-        },
-      ),
-    ],
-  ),
-),
-
       body: IndexedStack(
         index: _selectedIndex, // เปลี่ยนค่า index เมื่อกดปุ่ม
         children: <Widget>[
@@ -196,7 +195,7 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
+        selectedItemColor: Colors.black,
         onTap: _onItemTapped,
       ),
     );
